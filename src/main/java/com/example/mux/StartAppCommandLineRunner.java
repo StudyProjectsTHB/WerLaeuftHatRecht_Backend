e56260d0b212c1829fdc687ba6a67348b87d4fb9
einToast <laurin2102@gmail.com>
2024-07-17 00:43:10 +0200

    Change @gericht-brb.dummy to @gericht.brandenburg.dummy

--- src/main/java/com/example/mux/StartAppCommandLineRunner.java
+++ src/main/java/com/example/mux/StartAppCommandLineRunner.java
@@ -73,7 +73,7 @@ public class StartAppCommandLineRunner implements CommandLineRunner {
         ArrayList<User> users = new ArrayList<>();
         int numUsers = groups.size() * 10;
         for (int i = 0; i < numUsers; i++) {
-            users.add(new User("user" + i + "@gericht-brb.dummy", (i == 0) || (random.nextFloat() < 0.1)));
+            users.add(new User("user" + i + "@gericht.brandenburg.dummy", (i == 0) || (random.nextFloat() < 0.1)));
         }
         for (int i = 0; i < users.size(); i++) {
             User u = users.get(i);
@@ -124,7 +124,7 @@ public class StartAppCommandLineRunner implements CommandLineRunner {
         ArrayList<User> users = new ArrayList<>();
         int numUsers = groups.size() * 5;
         for (int i = 0; i < numUsers; i++) {
-            users.add(new User("user" + i + "@gericht-brb.dummy", (i == 0) || (i != 1 && random.nextFloat() < 0.1)));
+            users.add(new User("user" + i + "@gericht.brandenburg.dummy", (i == 0) || (i != 1 && random.nextFloat() < 0.1)));
         }
         for (int i = 0; i < users.size(); i++) {
             User u = users.get(i);
